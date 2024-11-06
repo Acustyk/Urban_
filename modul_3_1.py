924df21d78927b4f2327f5ae6e144b0ae3242e57
Acustyk <nik.v.kzr@gmail.com>
2024-11-06 23:07:12 +0300

    Add files via upload

---
+++ modul_3_1.py
@@ -0,0 +1,22 @@
+def count_calls ():   #подсчитывающая вызовы остальных функций.
+    global calls
+    calls +=1
+
+def string_info(stringInput = '' ):  # принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
+    count_calls()
+    return (len(stringInput),stringInput.upper(),stringInput.lower())
+
+
+def is_contains(stringInput = '' , listInput = []):  # принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+    count_calls()
+    for i in listInput:
+        if stringInput.upper() == i.upper():
+            return True
+    return False
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
