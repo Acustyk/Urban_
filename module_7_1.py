94cd7a3ac401f708ba01af27a5f06fedc03d17f3
Acustyk <nik.v.kzr@gmail.com>
2024-12-12 10:33:40 +0300

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,66 @@
+"""Задача "Учёт товаров":
+Необходимо реализовать 2 класса Product и Shop, с помощью которых будет производиться запись в файл с продуктами.
+Объекты класса Product будут создаваться следующим образом - Product('Potato', 50.0, 'Vagetables')
+и обладать следующими свойствами:
+Атрибут name - название продукта (строка).
+Атрибут weight - общий вес товара (дробное число) (5.4, 52.8 и т.п.).
+Атрибут category - категория товара (строка).
+Метод __str__, который возвращает строку в формате '<название>, <вес>, <категория>'.
+Все данные в строке разделены запятой с пробелами.
+"""
+'''   def __init__(self,name: str,weight: float,category: str):
+       self.name = name
+       self.weight = weight
+       self.category = category
+'''
+class Product:
+    def __init__(self, name: str, weight: float, category: str ):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        t=f' {self.name}, {self.weight}, {self.category} \n'
+        return t
+"""
+Объекты класса Shop будут создаваться следующим образом - Shop() и обладать следующими свойствами:
+Инкапсулированный атрибут __file_name = 'products.txt'.
+Метод get_products(self), который считывает всю информацию из файла __file_name, 
+закрывает его и возвращает единую строку со всеми товарами из файла __file_name.
+Метод add(self, *products), который принимает неограниченное количество объектов класса Product.
+ Добавляет в файл __file_name каждый продукт из products, если его ещё нет в файле (по названию). 
+ Если такой продукт уже есть, то не добавляет и выводит строку 'Продукт <название> уже есть в магазине' .
+"""
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name,"r")
+        txt = file.read()
+        file.close()
+        return txt
+
+    def add(self,*products):
+        file = open(self.__file_name,  "r")
+        txt = file.read()
+        file.close()
+        file = open(self.__file_name, "a")
+        for i in products:
+            #if i.name not in txt: # проверка на совпадение имени
+            if i.__str__() not in txt: # проверка на полное соответствие
+                file.write(i.__str__())
+                txt += i.__str__()
+            else:
+                print(f'Продукт {i.name} уже есть в магазине')
+        file.close()
+
+#Пример работы программы:
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+s1.add(p1, p2, p3)
+print(s1.get_products())
+
